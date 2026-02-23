2c53ae28c95074a4d19d86ef2cdfbcd07383fcf4
Owen Milota <randomhuman783@tuta.io>
2026-02-23 13:03:42 -0700

    server frame probably done-ish

--- fishserver.py
+++ fishserver.py
@@ -1,1 +1,24 @@
+import socket
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serverport = int(input("Enter port: "))
+
+server.bind(("0.0.0.0", serverport))
+
+server.listen(3)
+print(f"Listening for fish on port {serverport}")
+
+client, addr = server.accept()
+print(f"Client {addr} connected")
+
+while True:
+    msg = client.recv(2048)
+    if not msg:
+        break
+    print(f"Received {msg}")
+    client.send(msg.encode())
+
+
+client.close()
+server.close()
