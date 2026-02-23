f799b7c7b32e0f4248348b55ab883faeceda55e4
Owen Milota <randomhuman783@yahoo.com>
2026-02-23 13:43:08 -0700

    clean up the messages a bit, lose the `b""`/`b''`

--- fishserver.py
+++ fishserver.py
@@ -8,18 +8,18 @@ serverPort = int(input("Enter port: "))
 
 server.bind(("0.0.0.0", serverPort))
 
-server.listen(3)
+server.listen(1)
 print(f"Listening for fish on port {serverPort}")
 
 client, addr = server.accept()
 print(f"Fish {addr[0]} connected on port {addr[1]}")
 
 while True:
-    msg = client.recv(2048)
+    msg = client.recv(2048).decode()
     if not msg:
         break
     print(f"Received {msg}")
-    client.send(f"{msg}".encode())
+    client.send(msg.encode())
 
 
 client.close()
