d6f03c57f698ad1c0c9c55007766cac9eef4c620
tachyon <tachyon256@proton.me>
2026-02-27 13:09:44 -0700

    work on fix client not recieving asynchronously from sending, also new fihs

--- fishclient.py
+++ fishclient.py
@@ -1,37 +1,49 @@
 import socket
 import random
+import asyncio
 
 # Create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-defaultNames = ["TheFishMaster", "SomeFish", "RandomFish", "FishTheFourth", "TheFishinator", "Fysch", "Feesh"]
+defaultNames = ["TheFishMaster", "SomeFish", "RandomFish", "FishTheFourth", "TheFishinator", "Fysch", "Feesh",'bass (pronounced base)', 'bass (pronounced baas)', 'albacore','coelacanth','geoduck','golden tilefish','cancer borealis (crab species)','Lingcod','Northern Anchovy']
+def fishclient():
+    serverAddr = input("Input the IP of the server: ")
+    try:
+        serverPort = int(input("Input the port of the server: "))
+    except ValueError:
+        print('hey buddy thats not a port')
+        quit()
+    userName = input("Please input your username: ")
+    if userName == "":
+        userName = random.choice(defaultNames)
 
-serverAddr = input("Input the IP of the server: ")
-try:
-    serverPort = int(input("Input the port of the server: "))
-except ValueError:
-    print('hey buddy thats not a port')
-    quit()
-userName = input("Please input your username: ")
-if userName == "":
-    userName = random.choice(defaultNames)
+    # Connect to server (replace with the server machine’s IP if needed)
+    try:
+        client.connect((serverAddr, serverPort))
+    except ConnectionRefusedError:
+        print('Error: Connection Refused')
+        quit()
+    print("Connected to server")
+    client.send(f"..$CLNTMSG USERNAME: {userName}".encode())
 
-# Connect to server (replace with the server machine’s IP if needed)
-try:
-    client.connect((serverAddr, serverPort))
-except ConnectionRefusedError:
-    print('Error: Connection Refused')
-    quit()
-print("Connected to server")
-client.send(f"..$CLNTMSG USERNAME: {userName}".encode())
+    asyncio.run(sender())
+
+    msghandler()
+
 
 # Send messages and receive responses
-while True:
-    msg = input("Enter message: ")
-    if not msg:
-        break
-    client.send(msg.encode())
-    response = client.recv(1024).decode()
-    print(f"{response}")
+def msghandler():
+    while True:
+        response = client.recv(1024).decode()
+        print(f"{response}")
 
-client.close()
+
+async def sender():
+    while True:
+        msg = input("Enter message: ")
+        if not msg:
+            break
+        client.send(msg.encode())
+    client.close()
+
+fishclient()
